9a7cddfe4b8a10bc6f83d4fa08681274d2a5be60
pputyuen <pputyuen@student.42bangkok.com>
2025-06-23 23:19:40 +0000

    ...

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+openinput = input("What you gotta say? : ")
+while True:
+    want_input = input("I got that! Anything else? : ")
+    if want_input == "stop":
+        break
+print("I got that! Anything else? : STOP ")
