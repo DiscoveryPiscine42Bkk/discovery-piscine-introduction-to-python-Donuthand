d5cbd3f5b821ffb38711b7967e0bee938130cf0d
pputyuen <pputyuen@student.42bangkok.com>
2025-06-24 01:43:05 +0000

    ....

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,10 @@
+table = 0
+while table <= 10:
+    multiplier = 0
+    print(f"Table de {table} :" , end=' ') #end=' ' is create a space after it instead of a newline
+    while multiplier <= 10:
+        result = table * multiplier
+        print(result, end=' ')
+        multiplier += 1
+    print()
+    table +=1
